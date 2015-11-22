2e586552f2429797a25ea310b3f7be7ec9dab16c
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2015-11-22 15:40:44 +0100

    global: wrap mongo api
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- apps/mongopool/src/mongopool_app.erl
+++ apps/mongopool/src/mongopool_app.erl
@@ -20,7 +20,7 @@
 %% @end
 %%%-------------------------------------------------------------------
 
--module('mongopool_app').
+-module(mongopool_app).
 
 -author('Leonardo Rossi <leonardo.rossi@studenti.unipr.it>').
 
@@ -30,17 +30,125 @@
 -export([start/2
         ,stop/1]).
 
+-export([insert/3, update/4, update/5, delete/3, delete_one/3,
+         find_one/3, find_one/4, find/3, find/4, count/3, count/4,
+         ensure_index/3, command/2]).
+
 %%====================================================================
 %% API
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    'mongopool_sup':start_link().
+    mongopool_sup:start_link().
 
 %%--------------------------------------------------------------------
 stop(_State) ->
     ok.
 
 %%====================================================================
-%% Internal functions
+%% Internal functions: mongodb interface
 %%====================================================================
+
+%% @doc Insert a document into the collection.
+%%      Returns the document with an auto-generated _id if missing.
+-spec insert(term(), mongo:collection(), A) -> A.
+insert(PoolName, Collection, Doc) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:call(Worker, {insert, Collection, Doc})
+  end).
+
+%% @doc Update a document into the collection.
+%%      Returns the document with an auto-generated _id if missing.
+-spec update(
+        term(), mongo:collection(), mongo:selector(), bson:document()) -> ok.
+update(PoolName, Collection, Selector, Doc) ->
+  update(PoolName, Collection, Selector, Doc, []).
+
+%% @doc Update a document into the collection.
+%%      Returns the document with an auto-generated _id if missing.
+-spec update(term(), mongo:collection(), mongo:selector(),
+             bson:document(), proplists:proplist()) -> ok.
+update(PoolName, Collection, Selector, Doc, Args) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:cast(Worker, {update, Collection, Selector, Doc, Args})
+  end).
+
+%% @doc Delete selected documents
+-spec delete(term(), mongo:collection(), mongo:selector()) -> ok.
+delete(PoolName, Collection, Selector) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:cast(Worker, {delete, Collection, Selector})
+  end).
+
+%% @doc Delete first selected documents
+-spec delete_one(term(), mongo:collection(), mongo:selector()) -> ok.
+delete_one(PoolName, Collection, Selector) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:cast(Worker, {delete_one, Collection, Selector})
+  end).
+
+%% @doc Return first selected document, if any
+-spec find_one(term(), mongo:collection(),
+               mongo:selector()) -> {} | {bson:document()}.
+find_one(PoolName, Collection, Selector) ->
+  find_one(PoolName, Collection, Selector, []).
+
+%% @doc Return first selected document, if any
+-spec find_one(term(), mongo:collection(), mongo:selector(),
+               proplists:proplist()) -> {} | {bson:document()}.
+find_one(PoolName, Collection, Selector, Args) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:call(Worker, {find_one, Collection, Selector, Args})
+  end).
+
+%% @doc Return selected documents.
+-spec find(term(), mongo:collection(), mongo:selector()) -> mongo:cursor().
+find(PoolName, Collection, Selector) ->
+  find(PoolName, Collection, Selector, []).
+
+%% @doc Return selected documents.
+-spec find(term(), mongo:collection(), mongo:selector(),
+           proplists:proplist()) -> mongo:cursor().
+find(PoolName, Collection, Selector, Args) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:call(Worker, {find, Collection, Selector, Args})
+  end).
+
+%% @doc Count selected documents
+-spec count(term(), mongo:collection(), mongo:selector()) -> integer().
+count(PoolName, Collection, Selector) ->
+  count(PoolName, Collection, Selector, 0).
+
+%% @doc Count selected documents up to given max number; 0 means no max.
+%%      Ie. stops counting when max is reached to save processing time.
+-spec count(term(), mongo:collection(), mongo:selector(),
+            integer()) -> integer().
+count(PoolName, Collection, Selector, Limit) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:call(Worker, {count, Collection, Selector, Limit})
+  end).
+
+%% @doc Create index on collection according to given spec.
+%%      The key specification is a bson documents with the following fields:
+%%      key      :: bson document,
+%%                  for e.g.
+%%                  {field, 1, other, -1, location, 2d},
+%%                     <strong>required</strong>
+%%      name     :: bson:utf8()
+%%      unique   :: boolean()
+%%      dropDups :: boolean()
+-spec ensure_index(term(), mongo:collection(), bson:document()) -> ok.
+ensure_index(PoolName, Collection, IndexSpec) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:cast(Worker, {ensure_index, Collection, IndexSpec})
+  end).
+
+%% @doc Execute given MongoDB command and return its result.
+-spec command(term(), bson:document()) -> {boolean(),
+                                           bson:document()}. % Action
+command(PoolName, Command) ->
+  poolboy:transaction(PoolName, fun(Worker) ->
+    gen_server:call(Worker, {command, Command})
+  end).
+
+% TODO sync_command
---
+++ apps/mongopool/src/mongopool_worker.erl
@@ -0,0 +1,94 @@
+%%% @author Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
+%%% @copyright (C) 2015 Leonardo Rossi
+%%%
+%%% This software is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This software is distributed in the hope that it will be useful, but
+%%% WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this software; if not, write to the Free Software Foundation,
+%%% Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+%%%
+%%%-------------------------------------------------------------------
+%% @doc mongopool worker
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(mongopool_worker).
+
+-author('Leonardo Rossi <leonardo.rossi@studenti.unipr.it>').
+
+-behaviour(gen_server).
+-behaviour(poolboy_worker).
+
+-export([start_link/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+                 code_change/3]).
+
+-record(state, {connection}).
+
+start_link(Args) ->
+  gen_server:start_link(?MODULE, Args, []).
+
+init(Args) ->
+  process_flag(trap_exit, true),
+  application:ensure_all_started(mongodb),
+  Hostname = proplists:get_value(hostname, Args),
+  Database = proplists:get_value(database, Args),
+  Username = proplists:get_value(username, Args),
+  Password = proplists:get_value(password, Args),
+  {ok, Connection} = mongo:connect([{database, Database}, {host, Hostname},
+                  {login, Username}, {password, Password}]),
+  {ok, #state{connection=Connection}}.
+
+handle_call({find_one, Collection, Selector, Args}, _From,
+            #state{connection=Connection}=State) ->
+  {reply, mongo:find_one(Connection, Collection, Selector, Args), State};
+handle_call({find, Collection, Selector, Args}, _From,
+            #state{connection=Connection}=State) ->
+  {reply, mongo:find(Connection, Collection, Selector, Args), State};
+handle_call({count, Collection, Selector, Limit}, _From,
+            #state{connection=Connection}=State) ->
+  {reply, mongo:count(Connection, Collection, Selector, Limit), State};
+handle_call({command, Command}, _From, #state{connection=Connection}=State) ->
+  {reply, mongo:command(Connection, Command), State};
+handle_call(_Request, _From, State) ->
+  {reply, ok, State}.
+
+handle_cast({insert, Collection, Doc}, #state{connection=Connection}=State) ->
+  mongo:insert(Connection, Collection, Doc),
+  {noreply, State};
+handle_cast({update, Collection, Selector, Doc, Args},
+            #state{connection=Connection}=State) ->
+  mongo:update(Connection, Collection, Selector, Doc, Args),
+  {noreply, State};
+handle_cast({delete, Collection, Selector},
+            #state{connection=Connection}=State) ->
+  mongo:delete(Connection, Collection, Selector),
+  {noreply, State};
+handle_cast({delete_one, Collection, Selector},
+            #state{connection=Connection}=State) ->
+  mongo:delete_one(Connection, Collection, Selector),
+  {noreply, State};
+handle_cast({ensure_index, Collection, IndexSpec},
+            #state{connection=Connection}=State) ->
+  mongo:ensure_index(Connection, Collection, IndexSpec),
+  {noreply, State};
+handle_cast(_Msg, State) ->
+      {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, #state{connection=Connection}) ->
+  ok = mongodb:disconnect(Connection),
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
