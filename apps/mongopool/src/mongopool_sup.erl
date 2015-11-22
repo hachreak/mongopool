483d336db88e4c59f43d600992395617b7e911e2
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2015-11-22 12:47:20 +0100

    sup: load pool of mongopool_worker
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- apps/mongopool/src/mongopool_sup.erl
+++ apps/mongopool/src/mongopool_sup.erl
@@ -8,7 +8,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_link/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -22,13 +22,30 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
+start_link(Pools) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, Pools).
+
 %%====================================================================
 %% Supervisor callbacks
 %%====================================================================
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+  {ok, Pools} = application:get_env(mongopool, pools),
+  init(Pools);
+init(Pools) ->
+  RestartStrategy = {
+    one_for_all,
+    0, % MaxRestart,
+    1 % MaxTime
+  },
+  PoolSpecs = lists:map(
+                fun({Name, SizeArgs, WorkerArgs}) ->
+                    PoolArgs = [{name, {local, Name}},
+                                {worker_module, mongopool_worker}] ++ SizeArgs,
+                    poolboy:child_spec(Name, PoolArgs, WorkerArgs)
+                end, Pools),
+  {ok, {RestartStrategy, PoolSpecs}}.
 
 %%====================================================================
 %% Internal functions
